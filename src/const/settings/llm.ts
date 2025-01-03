c122e3aabdfe068ddd0888b99c352b3ff71ba23b
nannananananana <ni_nan_bupt@163.com>
2025-01-03 17:23:54 +0800

    delete-PROVIDER-env

--- src/const/settings/llm.ts
+++ src/const/settings/llm.ts
@@ -2,6 +2,7 @@
 // import { modelConfigSelectors } from '@/store/user/selectors';
 
 import { genUserLLMConfig } from '@/utils/genUserLLMConfig'
+import { DEFAULT_AGENT_CONFIG } from '@/const/settings';
 
 export const DEFAULT_LLM_CONFIG = genUserLLMConfig({
   qwen: {
@@ -18,14 +19,14 @@ let embeddingModelType = 'text-embedding-v3';
 let defaultModelType = 'qwen-max-latest';
 let defaultModelProvider = 'qwen';
 try {
-  if (process.env.PROVIDER === 'qwen'){
+  if (DEFAULT_AGENT_CONFIG.provider === 'qwen'){
     embeddingModelType = 'text-embedding-v3';
     defaultModelType = 'qwen-max-latest';
-    defaultModelProvider = 'qwen';
-  }else if (process.env.PROVIDER === 'openai'){
+    // defaultModelProvider = 'qwen';
+  }else if (DEFAULT_AGENT_CONFIG.provider === 'openai'){
     embeddingModelType = 'text-embedding-3-small';
     defaultModelType = 'gpt-4o-mini';
-    defaultModelProvider = 'openai';
+    // defaultModelProvider = 'openai';
   }
 }catch(err){
   console.error('❌ Provider type is nor supported:', err);
@@ -33,4 +34,5 @@ try {
 
 export const DEFAULT_EMBEDDING_MODEL = embeddingModelType;
 export const DEFAULT_MODEL = defaultModelType;
-export const DEFAULT_PROVIDER = defaultModelProvider;
+// export const DEFAULT_PROVIDER = defaultModelProvider;
+export const DEFAULT_PROVIDER = DEFAULT_AGENT_CONFIG.provider
